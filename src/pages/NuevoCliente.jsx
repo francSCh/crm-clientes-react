d1e2db35929c870bb61bbf0ef0f8692af1e47615
francSCh <salaschfr@gmail.com>
2022-10-04 21:09:25 -0500

    Mostrando el Formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Formulario from '../components/Formulario';
 
 function NuevoCliente() {
-  return (
-    <div>NuevoCliente</div>
-  )
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <h1 className='font-black text-4xl text-blue-900'>Nuevo Cliente</h1>
+            <p className='mt-3'>Llena todos los campos para registrar un nuevo Cliente</p>
+
+            <div className='flex justify-end'>
+                <button className='bg-blue-800 text-white px-3 py-1 font-bold uppercase' onClick={() => navigate(-1)}>
+                    Volver
+                </button>
+            </div>
+
+            <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-10'>
+                <form>
+                    <Formulario />
+
+                    <input type="submit" className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg' value='Registrar Cliente'/>
+                </form>
+            </div>
+        </>
+    )
 }
 
 export default NuevoCliente
